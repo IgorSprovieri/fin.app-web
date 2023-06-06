234ccae8add6eb4246fa234489d2e22d317c6ee1
Igor Sprovieri <igsp1@hotmail.com>
2023-06-06 11:54:26 -0300

    Feat: Global Styles

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,4 +1,18 @@
 import { colors } from './colors'
 import { extendTheme } from '@chakra-ui/react'
 
-export const theme = extendTheme({ colors })
+export const theme = extendTheme({
+  colors: {
+    brand: colors
+  },
+  styles: {
+    global: {
+      'html, body': {
+        fontFamily: 'Inter',
+        padding: 0,
+        margin: 0,
+        backgroundColor: colors.background
+      }
+    }
+  }
+})
