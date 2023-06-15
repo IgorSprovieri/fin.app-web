4554a0f3cf16e91078b99f0562208c6c31058d53
Igor Sprovieri <igsp1@hotmail.com>
2023-06-15 08:12:42 -0300

    Feat: Persist Redux

--- src/App.js
+++ src/App.js
@@ -3,35 +3,38 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from 'styles'
 import { AvatarScreen, HomeScreen, LoginScreen, RegisterScreen } from 'screens'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { store } from './services'
+import { store, persistor } from './services'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
 const App = () => {
   const queryClient = new QueryClient()
   return (
     <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <ChakraProvider theme={theme}>
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<LoginScreen></LoginScreen>}></Route>
-              <Route
-                path="/login"
-                element={<LoginScreen></LoginScreen>}
-              ></Route>
-              <Route
-                path="/register"
-                element={<RegisterScreen></RegisterScreen>}
-              ></Route>
-              <Route path="/home" element={<HomeScreen></HomeScreen>}></Route>
-              <Route
-                path="/avatar"
-                element={<AvatarScreen></AvatarScreen>}
-              ></Route>
-            </Routes>
-          </BrowserRouter>
-        </ChakraProvider>
-      </QueryClientProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <QueryClientProvider client={queryClient}>
+          <ChakraProvider theme={theme}>
+            <BrowserRouter>
+              <Routes>
+                <Route path="/" element={<LoginScreen></LoginScreen>}></Route>
+                <Route
+                  path="/login"
+                  element={<LoginScreen></LoginScreen>}
+                ></Route>
+                <Route
+                  path="/register"
+                  element={<RegisterScreen></RegisterScreen>}
+                ></Route>
+                <Route path="/home" element={<HomeScreen></HomeScreen>}></Route>
+                <Route
+                  path="/avatar"
+                  element={<AvatarScreen></AvatarScreen>}
+                ></Route>
+              </Routes>
+            </BrowserRouter>
+          </ChakraProvider>
+        </QueryClientProvider>
+      </PersistGate>
     </Provider>
   )
 }
--- src/services/store.js
+++ src/services/store.js
@@ -1,8 +1,39 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { setupListeners } from '@reduxjs/toolkit/query'
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import slice from './slice'
 
-export const store = configureStore({
-  reducer: {
-    counter: slice
-  }
+const persistConfig = {
+  key: 'root',
+  storage
+}
+
+const rootReducer = combineReducers({
+  counter: slice
 })
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+      }
+    })
+})
+
+setupListeners(store.dispatch)
+
+export const persistor = persistStore(store)
