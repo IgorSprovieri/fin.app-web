75c8abac5d1c4690980e0a26b2c2227a09d3462c
Igor Sprovieri <igsp1@hotmail.com>
2023-06-15 12:48:52 -0300

    Feat: Menu Selector

--- src/components/organisms/financesManager.js
+++ src/components/organisms/financesManager.js
@@ -4,49 +4,68 @@ import { useState } from 'react'
 
 export const FinancesManager = ({ finances }) => {
   const [mainTitle, setMainTitle] = useState('Transações')
+  const [menuSelector, setMenuSelector] = useState(1)
   const [modalIsOpen, setModalIsOpen] = useState(false)
   const [modalHeader, setModalHeader] = useState('')
-
-  const onClickHome = () => {
-    setMainTitle('Categorias')
-  }
-
-  const onClickTransitions = () => {
-    setMainTitle('Transações')
-  }
-
-  const onClickBalance = () => {
-    setMainTitle('Balanço')
-  }
-
-  const onClickAdd = () => {
-    setModalIsOpen(true)
-    setModalHeader('Adicionar')
-  }
 
   const onCloseModal = () => {
     setModalIsOpen(false)
   }
 
+  const ListFinances = ({ finances }) => {
+    return (
+      <>
+        {' '}
+        {finances.map((element) => (
+          <FinanceCard
+            key={element.id}
+            name={element.name}
+            value={element.value}
+            date={element.date}
+            category={element.category}
+          ></FinanceCard>
+        ))}
+      </>
+    )
+  }
+
   return (
     <>
       <Menu
-        onHome={() => onClickHome()}
-        onTransitions={() => onClickTransitions()}
-        onBalance={() => onClickBalance()}
-        onAdd={() => onClickAdd()}
+        onHome={() => {
+          setMainTitle('Categorias')
+          setMenuSelector(0)
+        }}
+        onTransitions={() => {
+          setMainTitle('Transações')
+          setMenuSelector(1)
+        }}
+        onBalance={() => {
+          setMainTitle('Balanço')
+          setMenuSelector(2)
+        }}
+        onAdd={() => {
+          setModalIsOpen(true)
+          setModalHeader('Adicionar')
+        }}
       ></Menu>
       <Main title={mainTitle}>
         <Flex w={'100%'} h={'100%'} flexDir={'column'} justify={'flex-start'}>
-          {finances.map((element) => (
-            <FinanceCard
-              key={element.id}
-              name={element.name}
-              value={element.value}
-              date={element.date}
-              category={element.category}
-            ></FinanceCard>
-          ))}
+          {menuSelector === 0 ? (
+            <ListFinances finances={finances}></ListFinances>
+          ) : (
+            <></>
+          )}
+          {menuSelector === 1 ? (
+            <ListFinances finances={finances}></ListFinances>
+          ) : (
+            <></>
+          )}
+          {menuSelector === 2 ? (
+            <ListFinances finances={finances}></ListFinances>
+          ) : (
+            <></>
+          )}
         </Flex>
       </Main>
       <Modal
